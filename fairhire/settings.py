07c7dfcebf404aa47a1d7286552a4129588a2151
Dhruv12 <64084228+Armour12@users.noreply.github.com>
2023-04-01 02:31:04 -0400

    Revert "removed google translate"

--- fairhire/settings.py
+++ fairhire/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fairhireapp',
-    
+    'google_translate',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
